586ccbbdc2b991e1ea2a150f2cdbd8c58a469268
aswin402 <aswin402@gmail.com>
2026-01-06 19:57:40 +0530

    feat: developing server now completed prisma.ts and auth.ts

---
+++ server/middleware/auth.ts
@@ -0,0 +1,13 @@
+import { stackServerApp } from "@/stack/server";
+import { Context, Next } from 'hono';
+
+export const authMiddleware = async (c: Context, next: Next) => {
+    const user = await stackServerApp.getUser();
+    
+    if (!user) {
+        return c.json({ error: 'Unauthorized' }, 401);
+    }
+
+    c.set('user', user);
+    await next();
+};
